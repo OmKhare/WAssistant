8d0c18fcabe4b39d357c63e31a72ad2b33a452cc
Om Khare <omkhare02@gmail.com>
2021-05-01 12:07:01 +0530

    Bug Fix

--- onlineDetector/urls.py
+++ onlineDetector/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.index, name='index'),
- path('result', views.result, name='add'),
- path('analysis', views.analysis, name='analysis')
+ path('', views.index, name='index'), #homeScreen
+ path('result', views.result, name='add'), #screen2
+ path('analysis', views.analysis, name='analysis') #screen3
 ]
--- onlineDetector/views.py
+++ onlineDetector/views.py
@@ -1,3 +1,4 @@
+# Importing the neccessary libraries
 from django.shortcuts import render
 from django.http import HttpResponse
 from selenium import webdriver
@@ -6,19 +7,24 @@ from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 import time
 
+# Function to create log file in .txt format
 def log_now(msg, name):
     with open("mylogs.txt", "a") as f:
         f.write(f"{name} {msg} {datetime.now()} \n")
 
+# Index function
 def index(request):
  return render(request, 'index.html')
 
+# Result function
 def result(request):
+ # Getting name and time duration
  name = request.GET['name']
  mins = int(request.GET['time'])
+
  chrome = webdriver.Chrome(ChromeDriverManager().install())
  chrome.get("https://web.whatsapp.com")
- time.sleep(10)
+ time.sleep(10) 
  search_box = chrome.find_element_by_xpath(
      "//*[@id=\"side\"]/div[1]/div/label/div/div[2]")
  search_box.send_keys(name)
@@ -39,6 +45,7 @@ def result(request):
 
  return render(request, 'result.html', {"result":name})
 
+# Analysis function
 def analysis(request):
  f = open("mylogs.txt", "r")
  f1 = f.readlines()
